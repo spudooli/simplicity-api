e5db335c5858b462badee214555a9331125890e5
Dave <dave@spudooli.com>
2017-09-29 09:23:32 +1300

    Getting access to my own data, no API be buggered

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # simplicity-api
 Simplicity Kiwisaver doesn't have an API so I'll get at my own data another way
 
-Uses Chrome Launcher - https://www.npmjs.com/package/chrome-launcher
-and Cromeless - https://github.com/graphcool/chromeless
+Nodejs with Chrome Launcher - https://www.npmjs.com/package/chrome-launcher
+and Chromeless - https://github.com/graphcool/chromeless
---
+++ simplicity.js
@@ -0,0 +1,38 @@
+
+const { Chromeless } = require('chromeless')
+
+const simplicityUsername = ""
+const simplicityPassword = ""
+
+const chromeLauncher = require('chrome-launcher');
+ 
+chromeLauncher.launch({
+	  startingUrl: 'https://google.com',
+	  chromeFlags: ['--headless', '--disable-gpu']
+});
+
+async function run() {
+	  const chromeless = new Chromeless()
+
+	  const html = await chromeless
+	    .goto('https://apisimplicity.mmcnz.co.nz/User/LogOn')
+	    .type(simplicityUsername, '#Username')
+	    .type(simplicityPassword, '#Password')
+	    .click('input[type="submit"]')
+	    .wait(15000)
+	    .html()
+	    .evaluate(function() {
+		var value =  document.querySelector('span.counter').innerHTML;
+		return value
+	    })
+	
+	  .goto('https://app.simplicity.kiwi//login/logout')
+
+	  console.log(html)
+
+	  await chromeless.end()
+}
+
+run().catch(console.error.bind(console))
+
+
